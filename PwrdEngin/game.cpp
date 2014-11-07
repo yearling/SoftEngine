8c3d487a87e2db82bcff0000d4fc01a114a5d36f
yyCom <yearlingwes@gamil.com>
2014-11-07 14:32:33 +0800

    change to vs2012

--- PwrdEngin/game.cpp
+++ PwrdEngin/game.cpp
@@ -39,8 +39,8 @@ namespace SoftEngine
 		//m_pFbxPaser->Load("E:\\scene_fbx\\test\\rectangle.fbx");
 		//m_pFbxPaser->Load("E:\\scene_fbx\\test\\box_texture.fbx");
 		//m_pFbxPaser->Load("E:\\scene_fbx\\test\\plane_texture.fbx");
-		m_pFbxPaser->Load("E:\\scene_fbx\\test\\tea.fbx");
-		//m_pFbxPaser->Load("E:\\scene_fbx\\test\\plane2X2.fbx");
+		//m_pFbxPaser->Load("E:\\scene_fbx\\test\\tea.fbx");
+		m_pFbxPaser->Load("E:\\scene_fbx\\test\\plane2X2.fbx");
 		//////////////////////////////////////////////////////////////////////////
 		m_pEasyCamera=new EASYCamera();
 		m_pEasyCamera->SetHWND(m_spMainWindow->m_hWnd);
@@ -52,7 +52,7 @@ namespace SoftEngine
 		m_pEasyCamera->SetViewParam(&eye,&at);
 		m_pEasyCamera->SetProjParam(PI*0.5f,(float)m_spMainWindow->m_iWidth/(float)
 			m_spMainWindow->m_iHeight,1.0f,1000.0f);
-		m_pBMP=BMPReader::GetInstance().LoadBMP("E:\\scene_fbx\\test\\bb.bmp");
+		m_pBMP=BMPReader::GetInstance().LoadBMP("E:\\scene_fbx\\test\\aa.bmp");
 		/*if(m_pBMP)
 		{
 			std::cout<<m_pBMP>width<<"		"<<m_pBMP>height<<std::endl;
@@ -78,8 +78,8 @@ namespace SoftEngine
 				m_pDevice->PSSetData();
 				m_pDevice->VSSetData();
 				//m_pDevice->SetCullMode(CULL_NONE);
-				m_pDevice->SetFillMode(FILL_SOLID);
-				//m_pDevice->SetFillMode(FILL_WIREFRAME);
+				//m_pDevice->SetFillMode(FILL_SOLID);
+				m_pDevice->SetFillMode(FILL_WIREFRAME);
 				m_pDevice->DrawIndexedPrimitive(PT_TRIANGLEIST,0,0,m_pFbxPaser->GetVertexNumber(),0,m_pFbxPaser->GetFaceNumber());
 				m_pDevice->EndScene();
 			}
